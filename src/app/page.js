aa7571e2da519dff84f350bd4540eca2dd4dacd3
Angelo <arlongoria93@gmail.com>
2023-08-01 19:14:55 -0500

    init

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen justify-center flex-col items-center  p-24">
+<div className='bg-white h-[641px] w-[928px] rounded-large'>test box</div>
+    </main>
+  )
+}
