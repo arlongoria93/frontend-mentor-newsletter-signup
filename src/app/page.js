c7f9d108cf2375870c9d58c36157767f4d5e6f20
Angelo <arlongoria93@gmail.com>
2023-08-01 19:16:50 -0500

    setting up main container styles

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen justify-center flex-col items-center  p-24">
-<div className='bg-white h-[641px] w-[928px] rounded-large'>test box</div>
+<div className='bg-white h-[641px] w-[928px] rounded-2xl'>test box</div>
     </main>
   )
 }
