6d94596828c6c8ec13e46d692306fefa6ae11a21
Angelo <arlongoria93@gmail.com>
2023-08-02 18:55:11 -0500

    update 7 files

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
-import Image from 'next/image'
+import Card from "../components/card";
+import "next/font/google";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center flex-col items-center  p-24">
-<div className='bg-white h-[641px] w-[928px] rounded-2xl'>test box</div>
+    <main className="flex min-h-screen justify-center flex-col items-center p-24">
+      <Card />
     </main>
-  )
+  );
 }
--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Roboto } from "next/font/google";
+import { IconList } from "./svgs";
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -10,7 +11,7 @@ const Card = () => {
   return (
     <div className="bg-white h-[641px] w-[928px] rounded-2xl flex flex-row">
       <div className="w-1/2 flex flex-col items-center justify-center">
-        <div className="w-[376px] h-[446px] border border-solid-black">
+        <div className="w-[376px] h-[446px] border border-solid-black flex flex-col gap-[24px]">
           <h1
             className={`text-[56px] font-bold ${roboto.className} text-[#242742]`}
           >
@@ -21,10 +22,36 @@ const Card = () => {
           >
             Join 60,000+ product managers receiving monthly updates on:
           </p>
-          <div className="flex flex-col">
-            <div className="flex flex-row items-center">
-              <p>Product discovery and building what matters</p>
+          <div className="flex flex-col gap-[10px]">
+            <div className="flex flex-row items-center gap-[16px]">
+              <IconList />
+              <p className={`${roboto.className} text-[16px] leading-6`}>
+                Product discovery and building what matters
+              </p>
             </div>
+            <div className="flex flex-row items-center gap-[16px]">
+              <IconList />
+              <p>Measuring to ensure updates are a success</p>
+            </div>
+            <div className="flex flex-row items-center gap-[16px]">
+              <IconList />
+              <p>And much more!</p>
+            </div>
+          </div>
+          <div className="w-[376px] h-[162px] flex flex-col">
+            <p
+              className={`text-[12px] leading-6 font-bold ${roboto.className}`}
+            >
+              Email address
+            </p>
+            <input
+              type="text"
+              className="w-[376px] h-[56px] border border-solid-black rounded-[8px] p-[16px]"
+              placeholder="email@company.com"
+            />
+            <button className="w-[376px] h-[56px] border border-solid-black rounded-[8px] p-[16px] bg-[#242742] text-[16px] font-bold leading-6 text-[#fff]">
+              Subscribe to monthly newsletter
+            </button>
           </div>
         </div>
       </div>
