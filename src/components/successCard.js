f2bae1882071f67ba237cdc2de7cba1f7e4bad91
Angelo <arlongoria93@gmail.com>
2023-08-03 00:28:57 -0500

    update successCard.js and src/components/svgs/index.js

--- src/components/successCard.js
+++ src/components/successCard.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { IconSuccess } from "./svgs";
 
-const SuccessCard = () => {
+const SuccessCard = ({ email }) => {
+  if (!email) return null;
   return (
-    <div className="w-[504px] h-[520px] bg-[#FFF] shadow-sm rounded-2xl flex flex-row">
-      Success
+    <div className="w-[504px] h-[520px] bg-[#FFF] shadow-sm rounded-2xl flex flex-col gap-[40px] p-[64px]">
+      <IconSuccess className="w-[64px] h-[64px] self-start" />
+      <div className="flex flex-col gap-[24px]">
+        <h1 className="text-[56px] font-bold text-[#242742]">
+          Thanks for subscribing!
+        </h1>
+        <p className="text-[16px] leading-6 text-[#242742]">
+          A confirmation email has been sent to{" "}
+          <span className="font-bold">{email}</span> Please open it and click
+          the button inside to confirm your subscription.
+        </p>
+      </div>
+      <button>Dismiss message</button>
     </div>
   );
 };
--- src/components/svgs/index.js
+++ src/components/svgs/index.js
@@ -1,2 +1,3 @@
 export { default as IconList } from "./icon-list.svg";
+export { default as IconSuccess } from "./icon-success.svg";
 export { default as IllustrationSignUpDesktop } from "./illustration-sign-up-desktop.svg";
